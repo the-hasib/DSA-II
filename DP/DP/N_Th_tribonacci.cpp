4d8fcf425d327cb364b35590418c5587e573155e
the-hasib <hasib.cse.uiu@gmail.com>
2025-09-03 11:07:40 +0600

    Add a program to find nth tribonacci

---
+++ DP/DP/N_Th_tribonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int tribonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+
+    int t0 = 0, t1 = 1, t2 = 1, tn;
+
+    for (int i = 3; i <= n; i++) {
+        tn = t0 + t1 + t2; 
+        t0 = t1;           
+        t1 = t2;
+        t2 = tn;
+    }
+
+    return tn;
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "T(" << n << ") = " << tribonacci(n) << endl;
+    return 0;
+}
