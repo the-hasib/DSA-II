3cc6739b8d85169e1f922218bd288615bd5146be
the-hasib <hasib.cse.uiu@gmail.com>
2025-09-16 21:46:13 +0600

    Erorr fixt

--- Divide and Conquer/Find_Sum_in_a_array.cpp
+++ Divide and Conquer/Find_Sum_in_a_array.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
 
+// Divide & Conquer sum
 int findSum(int arr[], int i, int j) {
-    if (i == j)
+    if (i == j) 
         return arr[i];
-    return arr[j] + findSum(arr, i, j - 1);
+
+    int mid = (i + j) / 2;
+
+    int leftSum = findSum(arr, i, mid);    
+    int rightSum = findSum(arr, mid + 1, j);
+
+    return leftSum + rightSum; 
 }
 
 int main() {
     int n;
-    cout<< "Enter input element size:";
+    cout << "Enter input element size: ";
     cin >> n;
-    cout <<"Enter your "<<n<<" number's :" ;
+
     int arr[n];
+    cout << "Enter your " << n << " numbers: ";
     for (int i = 0; i < n; i++) cin >> arr[i];
-    cout << findSum(arr, 0, n - 1) << endl;
+
+    cout << "Sum of array = " << findSum(arr, 0, n - 1) << endl;
+
+    return 0;
 }
