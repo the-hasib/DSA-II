54f8105232d4f561871f0b4d13d0b862ab0ce66e
the-hasib <hasib.cse.uiu@gmail.com>
2025-09-16 22:01:12 +0600

    Add a program to calculate  odd number of sum

---
+++ Divide and Conquer/Odd_Sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int D(const vector<int>& arr, int i, int j) {
+    if (i == j) {
+        if (arr[i] % 2 != 0) return arr[i]; // odd number check
+        return 0;
+    }
+    int mid = (i + j) / 2;
+    int leftSum = D(arr, i, mid);
+    int rightSum = D(arr, mid + 1, j);
+    return leftSum + rightSum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    cout << D(arr, 0, n - 1) << endl;
+    return 0;
+}
