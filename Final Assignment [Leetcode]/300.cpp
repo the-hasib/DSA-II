9b7c1dbe9f3de02213dc394b4cc910c2c1b2e681
the-hasib <hasib.cse.uiu@gmail.com>
2025-10-21 20:07:02 +0600

    Add a code

---
+++ Final Assignment [Leetcode]/300.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int n;
+    int t[2500][2500];
+    int solve(vector<int> &nums, int i, int P) {
+        if (i == n) return 0;
+        if (P != -1 && t[i][P] != -1) return t[i][P];
+        int take = 0;
+        if (P == -1 || nums[i] > nums[P]) {
+            take = 1 + solve(nums, i+1, i);
+        }
+        int skip = solve(nums, i+1, P);
+        if (P != -1) t[i][P] = max(skip, take);
+        return max(skip, take);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        n = nums.size();
+        memset(t, -1, sizeof(t));
+        return solve(nums,0,-1);    
+    }
+};
