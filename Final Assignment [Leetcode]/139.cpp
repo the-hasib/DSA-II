75a40fc4179148d2f6718f3435865a2c1e4c7608
the-hasib <hasib.cse.uiu@gmail.com>
2025-10-21 20:08:27 +0600

    Add a code

---
+++ Final Assignment [Leetcode]/139.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, false);
+        dp[s.size()] = true;
+
+        for (int i=s.size()-1; i >= 0; i--) {
+            for (const string& word : wordDict){
+                if (word == s.substr(i, word.size())) {
+                    dp[i] = dp[i + word.size()];
+                    if (dp[i]) break;
+                }
+            } 
+        }
+        return dp[0];
+    }
+};
